1164e22b88226089a9a9c27aca881d2b081a0762
lparolari <luca.parolari23@gmail.com>
2018-07-21 14:33:10 +0200

    Add base backup algorithm

--- src/mbt/Backup.py
+++ src/mbt/Backup.py
@@ -2,26 +2,85 @@ import zlib
 import zipfile
 from os import path
 from os import walk
+import datetime
+import os
 
 
 class Backup(object):
+    COMP_TARGZ = 'targz'
+    COMP_ZIP = 'zip'
+
     _exec_path = None
     _backup_paths = None
-    _out_archive_name = None
+    _user_out_archive_name = None
+    _compression = None
+    _tmp_archive_name = None
 
-    def __init__(self, exec_path, backup_paths=None, out_archive_name=None, logs_enabled=True):
+    def __init__(self, exec_path, backup_paths=None, out_archive_name=None, compression=COMP_TARGZ, logs_enabled=True):
         self._exec_path = exec_path
         self._backup_paths = backup_paths
-        if out_archive_name is None:
-            self._out_archive_name = 'default.zip'
+        self._user_out_archive_name = out_archive_name
+        self._compression = compression
+        self._tmp_archive_name = "bck_{}.tmp".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
         assert self._ok()
 
     def backup(self):
-        z = zipfile.ZipFile(self._out_archive_name, 'w', zipfile.ZIP_DEFLATED)
-        # Implement here backup logic.
+        """
+        Executes the backup process.
+        Returns:
+            Void.
+        """
+        zip_out = zipfile.ZipFile(self._tmp_archive_name, 'w', zipfile.ZIP_DEFLATED)
 
-    def _ok(self):
-        ok = self._exec_path is not None
+        # Iterating thought folder trees.
+        for bpath in self._backup_paths:
+            # Walk visit all folders recursively
+            for (folder, subfolders, files) in os.walk(bpath):
+                zip_out.write(folder)  # Write the path to the zip.
+                for file in files:
+                    # Add files in right path position.
+                    zip_out.write(os.path.join(folder, file))
+
+        zip_out.close()
+        self._on_finish()
+
+    def _on_finish(self):
+        os.rename(self._tmp_archive_name, self._out_archive_name())
+
+    def _out_archive_name(self):
+        """
+        Calculates archive name based on user input.
+        Returns:
+            The archive name.
+        """
+        if self._user_out_archive_name is not None:
+            return self._user_out_archive_name
+        else:
+            now = datetime.datetime.now()
+            return 'bck_' + now.strftime("%Y%m%d-%H%M%S") + '.' + self._get_extension()
+
+    def _get_extension(self):
+        """
+        Gets archive's extension based on chosen compression.
+        Returns:
+            The archive extension based on compression.
+        """
+        if self._compression == self.COMP_TARGZ:
+            return 'tar.gz'
+        else:
+            return 'zip'
+
+    def _ok(self) -> bool:
+        """
+        Checks class invariant.
+        Returns:
+            True if invariant is ok, false otherwise.
+        """
+        ok = True
+        ok = ok and self._exec_path is not None
+        ok = ok and self._compression in [self.COMP_TARGZ, self.COMP_ZIP]
+        ok = ok and self._tmp_archive_name is not None
         return ok
 
 # fh = logging.FileHandler(r'/path/to/log.txt')
--- src/mbt/__main__.py
+++ src/mbt/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 import mbt
 from mbt import __version__, Backup
@@ -12,7 +13,11 @@ def get_parser():
     parser = argparse.ArgumentParser('micro-backup-tool')
     version = '%(prog)s ' + __version__
     parser.add_argument('--version', '-v', action='version', version=version)
-    parser.add_argument('--backup_paths', '-b', nargs='*')
+    parser.add_argument('--backup_paths', '-b', default=None, nargs='*', help='Paths to backup')
+    parser.add_argument('--compression', '-c', default='targz', choices=['targz', 'zip'], help='The compression format')
+    parser.add_argument('--logs_disabled', '-l', default=False, help='Disable logging')
+    parser.add_argument('--errorlogs_disabled', '-e', default=False, help='Disable error logging')
+    parser.add_argument('--unbacked_list_disabled', '-u', default=False, help='Disable unbacked-up files list')
     return parser
 
 
@@ -28,16 +33,19 @@ def main(args=None):
 
     parser = get_parser()
     args = parser.parse_args(args)
-    print(args.backup_paths)
+    print(args)
 
-    exec_path = repr(os.getcwd())
-    exec_path = exec_path.replace('\'', '')
-    print(exec_path)
-    b = Backup(exec_path=exec_path, backup_paths=args.backup_paths)
+    exec_path = os.getcwd()
+    backup_paths = args.backup_paths
+    compression = args.compression
+
+    b = Backup(exec_path=exec_path,
+               backup_paths=backup_paths,
+               compression=compression)
     b.backup()
 
     # Put your main script logic here
-    print('No action defined for mbt module!')
+    # print('No action defined for mbt module!')
 
 
 if __name__ == '__main__':
